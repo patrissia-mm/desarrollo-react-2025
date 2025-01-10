2f7e4a8178e17fa5ef265886730edd83b886b766
patrissia-mm <patriciamedinameneses@gmail.com>
2025-01-09 21:33:24 -0400

    Añadiendo counter al home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -6,10 +6,13 @@ import reactLogo from "../assets/react.svg";
 import viteLogo from "../assets/vite.svg";
 import gitLogo from "../assets/github.svg";
 
+import { useSelector, useDispatch } from 'react-redux'
+
 const Home =()=>{
+  const count = useSelector((state) => state.counter.value)
     return(
         <>
-
+          Count value: <span>{count}</span>
           <OpenLink redirectURL="https://vite.dev"  logoImage={viteLogo} />
           <OpenLink redirectURL="https://react.dev"  logoImage={reactLogo} />
           <OpenLink redirectURL="https://github.com/"  logoImage={gitLogo} />
